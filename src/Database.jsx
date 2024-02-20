8d739f710d465bad7efddf7f23f16789350616ed
Lana Gomes <lana.gomes1919@gmail.com>
2024-02-20 19:34:43 -0300

    projects in database updated

---
+++ src/Database.jsx
@@ -0,0 +1,25 @@
+import cat from "./images/cat.jpg";
+import happy from "./images/happy.png";
+import lyme from "./images/lyme1.jpg";
+
+export const projects = [
+  {
+    id: 1,
+    projectImg: { cat },
+    projectName: "Marka",
+    technologiesUsed: "VS Code  Sublime Text   Atom  iTerm2   Hyper",
+  },
+  {
+    id: 2,
+    projectImg: { happy },
+    projectName: "Marka2",
+    technologiesUsed: "VS Code  Sublime Text   Atom  iTerm2   Hyper",
+  },
+
+  {
+    id: 3,
+    projectImg: { lyme },
+    projectName: "Marka3",
+    technologiesUsed: "VS Code  Sublime Text   Atom  iTerm2   Hyper",
+  },
+];
