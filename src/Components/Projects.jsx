e1d2be171e665a3c787caa728b7e27accaa1eacd
Lana Gomes <lana.gomes1919@gmail.com>
2024-02-20 21:09:18 -0300

    project layout done

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,4 +1,29 @@
+//import { projects } from "../Database";
+import cat from "../images/cat.jpg";
+import githubIcon from "../images/githubIcon.png";
+import externalLinkIcon from "../images/externalLinkIcon.png";
+
 function Projects() {
-  return <div>Projects</div>;
+  return (
+    <div className="wrapperProjects">
+      <h1>Projetos</h1>
+      <section className="cardProject">
+        <img src={cat} />
+        <div className="container">
+          <h3>Resumo do título do projeto</h3>
+          <h4>Tecnologias utilizadas</h4>
+          <p>VS Code |Sublime |Text | Atom iTerm2 | Hyper</p>
+        </div>
+        <div className="projectIcons">
+          <a>
+            <img src={githubIcon}></img>
+          </a>
+          <a>
+            <img src={externalLinkIcon}></img>
+          </a>
+        </div>
+      </section>
+    </div>
+  );
 }
 export default Projects;
